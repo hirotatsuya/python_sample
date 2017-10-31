7969ec088f599dd5144e623aa7c1a440ff8a5254
tatsuya <tatsuya_hirota@kronos.jp>
2017-10-31 14:54:14 +0900

    study src

---
+++ src/sample.py
@@ -0,0 +1,4 @@
+total 3
+-rw-r--r-- 1 tatsuya_hirota 197121 104 10ŒŽ 31 14:32 file.py
+-rw-r--r-- 1 tatsuya_hirota 197121  16 10ŒŽ 31 14:32 sample.py
+-rw-r--r-- 1 tatsuya_hirota 197121 212 10ŒŽ 31 14:53 shell_script.py
