e82532b8ae1c68ba8c6e66cc9842825378a2bb4a
tatsuya <tatsuya_hirota@kronos.jp>
2017-12-14 14:15:34 +0900

    update

--- selenium/phantomjs_sample.py
+++ selenium/phantomjs_sample.py
@@ -4,11 +4,13 @@ from bs4 import BeautifulSoup
 import re
 import lxml
 
-driver = webdriver.PhantomJS(executable_path='./phantomjs')
+
+# path = './phantomjs'
+path = '../node_modules/phantomjs/lib/phantom/bin/phantomjs'
+path = '../node_modules/phantomjs/bin/phantomjs'
+driver = webdriver.PhantomJS(executable_path=path)
 driver.get("https://coincheck.com/ja/exchange")
 source = driver.page_source
 ripple = BeautifulSoup(source, 'lxml').find_all(class_=re.compile('currency_desc ng-binding'))[7].text
 print(ripple)
 driver.quit()
-
-
