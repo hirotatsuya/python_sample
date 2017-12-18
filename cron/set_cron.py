c97631663f3fc6a96847f5c2e115fc975e9e7721
tatsuya <tatsuya_hirota@kronos.jp>
2017-12-18 11:05:41 +0900

    update

--- cron/set_cron.py
+++ cron/set_cron.py
@@ -10,14 +10,17 @@ import slackweb
 import lxml
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from os import path
+
+APP_ROOT = path.dirname(path.dirname(path.abspath(__file__)))
 
 def get_ripple():
   slack = slackweb.Slack(url='https://hooks.slack.com/services/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')
 
   target_url = 'https://coincheck.com/ja/exchange'
 
-  # phantomjs_path = '../node_modules/phantomjs/lib/phantom/bin/phantomjs'
-  phantomjs_path = '../node_modules/phantomjs/bin/phantomjs'
+  phantomjs_path = APP_ROOT + '/node_modules/phantomjs/lib/phantom/bin/phantomjs' # win
+  # phantomjs_path = APP_ROOT + '/node_modules/phantomjs/bin/phantomjs' # mac
 
   try:
     browser = webdriver.PhantomJS(executable_path=phantomjs_path)
